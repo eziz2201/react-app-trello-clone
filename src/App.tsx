6a776904fe4c6a61fd427d70c62de2a22b3f1801
eziz2201 <e_myradow@mail.ru>
2022-06-14 19:01:52 +0300

    feat(app) add reactJs

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { StyledApp } from "./AppStyles";
+import Header from "./componenst/Header/Header";
+
+
+
+function App() {
+  return (
+    <StyledApp>
+      <Header/>
+    </StyledApp>
+  );
+}
+
+export default App;
---
+++ src/AppStyles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+  max-width: 1920px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 20px;
+`;
+
+export { StyledApp };
